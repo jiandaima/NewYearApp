000fc78f348af0c31454890fcbac7eeba72a7741
UserX <KVA_Kudzu@Mail.ru>
2013-01-05 18:09:34 +0400

    scalable tabbar_background was added

--- gen/com/viewpagerindicator/R.java
+++ gen/com/viewpagerindicator/R.java
@@ -99,13 +99,13 @@ public final class R {
 		public static final int LinePageIndicator_centered = 1;
 	}
 	public static final class drawable {
-		public static final int vpi__tab_selected_pressed_holo = 0x7f020063;
-		public static final int vpi__tab_selected_focused_holo = 0x7f020061;
-		public static final int vpi__tab_selected_holo = 0x7f020062;
-		public static final int vpi__tab_unselected_focused_holo = 0x7f020064;
-		public static final int vpi__tab_unselected_holo = 0x7f020065;
-		public static final int vpi__tab_indicator = 0x7f020060;
-		public static final int vpi__tab_unselected_pressed_holo = 0x7f020066;
+		public static final int vpi__tab_selected_pressed_holo = 0x7f020064;
+		public static final int vpi__tab_selected_focused_holo = 0x7f020062;
+		public static final int vpi__tab_selected_holo = 0x7f020063;
+		public static final int vpi__tab_unselected_focused_holo = 0x7f020065;
+		public static final int vpi__tab_unselected_holo = 0x7f020066;
+		public static final int vpi__tab_indicator = 0x7f020061;
+		public static final int vpi__tab_unselected_pressed_holo = 0x7f020067;
 	}
 	public static final class attr {
 		public static final int footerIndicatorStyle = 0x7f010014;
