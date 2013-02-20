a28f0f24e417d30e7c52829ec973d2d393e46b37
UserX <kva_kudzu@mail.ru>
2013-02-20 12:15:01 +0400

    - app killing, + InputStreamReader

--- App/src/org/uDevelop/newyearapp/CategoryInfo.java
+++
@@ -1,7 +0,0 @@
-package org.uDevelop.newyearapp;
-
-public class CategoryInfo {
-	public String name;
-	public String icon;
-
-}
--- App/src/org/uDevelop/newyearapp/ItemInfo.java
+++
@@ -1,9 +0,0 @@
-package org.uDevelop.newyearapp;
-
-public class ItemInfo {
-	public String name;
-	public String icon;
-	public String picture; //Та большая фотка, что перед текстом
-	public String text;
-
-}
